be38d55645c511a09be98f8659d54f90028c6d3f
nikitakolhe9 <99472484+nikitakolhe9@users.noreply.github.com>
2022-02-11 15:06:03 +0530

    Add files via upload

---
+++ Brainy Quiz/js/questions.js
@@ -0,0 +1,58 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "A HTML document can contain_____",
+    answer: "All of the above",
+    options: [
+      "Attributes",
+      "Tags",
+      "Raw text",
+      "All of the above",
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "A page designed in html is called?",
+    answer: "Web page",
+    options: [
+    "Application",
+    "Cover Page",
+    "Front End",
+    "Web page",
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+];
